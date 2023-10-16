b285f6a3438727c565758d882a2990d82d3478db
Marcelo Hernandez <marcelohdez.inq@gmail.com>
2023-10-16 01:54:39 -0400

    reorganize code

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,85 @@
+use std::{
+    env,
+    ffi::OsStr,
+    fs,
+    path::{Path, PathBuf},
+    process::Command,
+};
+
+use anyhow::{bail, Ok};
 use clap::Parser;
+use directories_next::ProjectDirs;
 
 #[derive(Debug, Parser)]
 #[command(author, version, about)]
 pub struct Atmpt {
     pub template: String,
+}
+
+impl Atmpt {
+    pub fn parse_with(editor: &OsStr, dirs: &ProjectDirs) -> anyhow::Result<()> {
+        let args = Self::parse();
+        let template = &args.template;
+
+        let templates = get_templates(dirs)?;
+        let wanted_dir = get_dir(template, &templates)?;
+
+        let tmp_dir = env::temp_dir().join(template);
+        copy_dir_recursively(wanted_dir, &tmp_dir)?;
+
+        std::env::set_current_dir(&tmp_dir).expect("Could not change to temp directory!");
+        Command::new(editor)
+            .arg(&tmp_dir)
+            .spawn()?
+            .wait()
+            .expect("Could not launch editor!");
+
+        Ok(())
+    }
+}
+
+fn get_templates(dirs: &ProjectDirs) -> anyhow::Result<Vec<PathBuf>> {
+    let mut templates = Vec::new();
+    // should be ~/.local/share/atmpt/* on a linux system
+    for entry in fs::read_dir(dirs.data_dir())? {
+        let path = entry?.path();
+
+        if path.is_dir() {
+            templates.push(path);
+        }
+    }
+
+    Ok(templates)
+}
+
+fn get_dir<'a>(name: &str, list: &'a [PathBuf]) -> anyhow::Result<&'a Path> {
+    for path in list {
+        if let Some(str) = path.file_name().and_then(|str| str.to_str()) {
+            if str == name {
+                return Ok(path);
+            }
+        }
+    }
+
+    bail!(format!(
+        "No template '{name}' exists! Templates:\n{list:#?}"
+    ))
+}
+
+// modified from https://stackoverflow.com/a/65192210/15425442
+fn copy_dir_recursively(from: &Path, to: &Path) -> anyhow::Result<()> {
+    fs::create_dir(to)?;
+
+    for entry in fs::read_dir(from)? {
+        let entry = entry?;
+        let path = entry.path();
+
+        if path.is_dir() {
+            copy_dir_recursively(&path, &to.join(entry.file_name()))?;
+        } else {
+            fs::copy(path, to.join(entry.file_name()))?;
+        }
+    }
+
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,102 +1,26 @@
-use std::{
-    fs,
-    path::{Path, PathBuf},
-    process::Command,
-};
+use std::ffi::{OsStr, OsString};
 
-use anyhow::{anyhow, Ok};
+use anyhow::{bail, Ok};
 use atmpt::Atmpt;
-use clap::Parser;
 use directories_next::ProjectDirs;
 
 fn main() -> anyhow::Result<()> {
-    let editor_env_pair = std::env::vars_os().find(|(k, _)| {
-        if let Some(str) = k.to_str() {
-            return str == "EDITOR";
-        }
-        false
-    });
-
-    let Some((_, editor)) = editor_env_pair else {
-        return Err(anyhow!("$EDITOR environment variable is not set!"));
-    };
-    let Some(editor) = editor.to_str() else {
-        return Err(anyhow!(
-            "$EDITOR environment variable is not valid Unicode?"
-        ));
-    };
-
-    let args = Atmpt::parse();
+    let editor = get_editor()?;
     let Some(dirs) = ProjectDirs::from("me", "marcelohdez", "Atmpt") else {
-        return Err(anyhow!("Could not get project dirs!"));
+        bail!("Could not generate any directories for this OS!");
     };
 
-    let mut wanted_template = None;
-    let templates = match get_templates(&dirs) {
-        anyhow::Result::Ok(v) => v,
-        Err(_) => Vec::new(),
-    };
-
-    for path in &templates {
-        if let Some(str) = path.file_name().and_then(|name| name.to_str()) {
-            if str == args.template {
-                wanted_template = Some(path);
-                println!("Creating project from template \'{str}\'");
-            }
-        }
-    }
-
-    let template = args.template;
-    let Some(template_dir) = wanted_template else {
-        return Err(anyhow!(
-            "The template \'{template}\' does not exist! Found templates:\n{templates:#?}"
-        ));
-    };
-    let tmp_dir = format!("/tmp/{template}");
-
-    copy_dir_all(template_dir, &tmp_dir)?;
-
-    println!("Moving to temporary project");
-    std::env::set_current_dir(&tmp_dir).expect("Could not change to temp directory!");
-
-    println!("Opening {editor}...");
-    Command::new(editor)
-        .arg(&tmp_dir)
-        .spawn()?
-        .wait()
-        .expect("Failed to wait for editor!");
-
-    Ok(())
+    Atmpt::parse_with(&editor, &dirs)
 }
 
-// copied from https://stackoverflow.com/a/65192210/15425442
-fn copy_dir_all(from: impl AsRef<Path>, to: impl AsRef<Path>) -> anyhow::Result<()> {
-    fs::create_dir(&to)?;
+fn get_editor() -> anyhow::Result<OsString> {
+    let editor_key = OsStr::new("EDITOR");
 
-    for entry in fs::read_dir(from)? {
-        let entry = entry?;
-        let path = entry.path();
-
-        if path.is_dir() {
-            copy_dir_all(path, to.as_ref().join(entry.file_name()))?;
-        } else {
-            fs::copy(path, to.as_ref().join(entry.file_name()))?;
+    for (k, v) in std::env::vars_os() {
+        if k == editor_key {
+            return Ok(v);
         }
     }
 
-    Ok(())
-}
-
-fn get_templates(dirs: &ProjectDirs) -> anyhow::Result<Vec<PathBuf>> {
-    let mut templates = Vec::new();
-    // should be ~/.local/share/atmpt/*
-    for entry in fs::read_dir(dirs.data_dir())? {
-        let path = entry?.path();
-
-        if path.is_dir() {
-            templates.push(path);
-        }
-    }
-
-    Ok(templates)
+    bail!("No editor could be found! Please set your $EDITOR environment variable")
 }
